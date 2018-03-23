28e6501aed6cb9d9e25bb00a9fb9682fb8e0c395
Thang Nguyen <sexinmyeye@gmail.com>
2018-03-23 12:58:09 -0700

    4thDoIt
    add dialog, function update statusID in OrderingDetail
    add 2 drawable to build dialog
    update OrderingDetail screen font size
    add new function DAO to update Status

--- AndroidProject/app/src/main/java/com/example/admin/androidproject/OrderingDetail.java
+++ AndroidProject/app/src/main/java/com/example/admin/androidproject/OrderingDetail.java
@@ -1,6 +1,12 @@
 package com.example.admin.androidproject;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +31,7 @@ public class OrderingDetail extends AppCompatActivity {
     TextView employeeNameTV = null;
     TextView orderTimeTV = null;
     TextView totalBillTV = null;
-
+    ProductViewDAO dao = new ProductViewDAO();
 
 
     @Override
@@ -51,7 +57,7 @@ public class OrderingDetail extends AppCompatActivity {
 
         listFood = new ArrayList<>();
         try {
-            listFood = new ProductViewDAO().getFoodForOrder(orderID);
+            listFood = dao.getFoodForOrder(orderID);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -74,6 +80,57 @@ public class OrderingDetail extends AppCompatActivity {
         employeeNameTV.setText(employeeName+"");
         orderTimeTV.setText(orderTime+"");
 
+        payBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+// Add the buttons
+                builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        boolean isUpdate = false;
+                        try {
+                            isUpdate = dao.updateOrderStatus(orderID);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        Intent result = new Intent();
+                        result.putExtra("isUpdate", isUpdate);
+                        setResult(Activity.RESULT_OK, result);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(R.string.notyet, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.setTitle("Payment Confirm");
+                builder.setMessage("Do you want to pay this order?");
+                AlertDialog alertDialog = builder.create();
+                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.LTGRAY));
+                alertDialog.show();
+                Button nbutton = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+                nbutton.setBackground(
+                        ContextCompat.getDrawable(
+                                getBaseContext(),R.drawable.dialog_button
+                        ));
+                nbutton.setTextColor(Color.BLACK);
+                Button pbutton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+                pbutton.setBackground(
+                        ContextCompat.getDrawable(
+                                getBaseContext(),R.drawable.dialog_button
+                        ));
+                pbutton.setTextColor(Color.BLACK);
+
+//                final Dialog dialog = new Dialog(view.getContext()); // Context, this, etc.
+//                dialog.setContentView(R.layout.dialog_confirm);
+//                TextView dialogContent = (TextView) findViewById(R.id.dialog_info);
+//                dialogContent.setText("Do you sure to pay this order?");
+//                dialog.setTitle("Hello");
+//                dialog.show();
+            }
+        });
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
